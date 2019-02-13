7fdf7fac568891b0256de389c01dbccd43cbc040
tlamont06 La <tlamont06@gmail.com>
2019-02-13 19:10:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+    var lowercase = 'hello'
+  if(string.toLowerCase() === lowercase){
+    return "I can't hear you!"
+  }
+}
+function sayHiToGrandma(string) {
+  var uppercase = 'HELLO'
+  if (string.uppercase === uppercase) {
+    return 'YES INDEED!'
+  }
+}
