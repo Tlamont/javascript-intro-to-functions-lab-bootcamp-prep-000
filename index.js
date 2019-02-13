52c879f4546ba325eb6665147942f5a6b9baf1cd
tlamont06 La <tlamont06@gmail.com>
2019-02-13 19:14:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }else if(string.toUpperCase() === uppercase){
     return 'YES INDEED!'
+  }else{
+    return "I love you, too!"
   }
 }
 
